e268ae6f645a715160de535d65cd4875c2b9db41
eline <1096534@hr.nl>
2025-05-06 16:07:44 +0200

    fishing game

--- src/js/game.js
+++ src/js/game.js
@@ -5,27 +5,93 @@ import { Resources, ResourceLoader } from './resources.js'
 export class Game extends Engine {
 
     constructor() {
-        super({ 
-            width: 1280,
-            height: 720,
+        super({
+            width: 800,
+            height: 450,
             maxFps: 60,
             displayMode: DisplayMode.FitScreen
-         })
+        })
         this.start(ResourceLoader).then(() => this.startGame())
     }
 
     startGame() {
-        console.log("start de game!")
-        const fish = new Actor()
-        fish.graphics.use(Resources.Fish.toSprite())
-        fish.pos = new Vector(500, 300)
-        fish.vel = new Vector(-10,0)
-        fish.events.on("exitviewport", (e) => this.fishLeft(e))
-        this.add(fish)
+
+        for (let i = 0; i < 50; i++) {
+            let x = Math.random() * 1200
+            let y = Math.random() * 450
+
+            console.log("start de game!")
+            const fish = new Actor()
+            fish.graphics.use(Resources.Fish.toSprite())
+
+            let sc = Math.random() * 2 + 0.2
+            fish.scale = new Vector(sc, sc)
+
+            fish.pos = new Vector(x, y)
+            fish.vel = new Vector(-100, 0)
+
+            fish.events.on("exitviewport", (e) => this.fishLeft(e))
+            this.add(fish)
+        }
+
+        
+        for (let i = 0; i < 10; i++) {
+            let x = Math.random() * -1200
+            let y = Math.random() * 450
+
+            console.log("start de game!")
+            const shark = new Actor()
+            shark.graphics.use(Resources.Shark.toSprite())
+
+            // let sc = Math.random() * 2 + 0.2
+            shark.scale = new Vector(0.2, 0.2)
+
+            shark.pos = new Vector(x, y)
+            shark.vel = new Vector(200, 0)
+
+            shark.events.on("exitviewport", (e) => this.sharkLeft(e))
+            this.add(shark)
+        }
+
+        for (let i = 0; i < 10; i++) {
+            let x = Math.random() * 1200
+            let y = 600 + Math.random() * 100
+
+            console.log("start de game!")
+            const bubble = new Actor()
+            bubble.graphics.use(Resources.Bubble.toSprite())
+
+            let sc = Math.random() * 2 + 0.2
+            bubble.scale = new Vector(sc, sc)
+
+            bubble.pos = new Vector(x, y)
+            bubble.vel = new Vector(0,-100)
+
+            bubble.events.on("exitviewport", (e) => this.bubbleLeft(e))
+            this.add(bubble)
+        }
+
     }
 
     fishLeft(e) {
-        e.target.pos = new Vector(1350, 300)
+        let x = Math.random() * 1200
+        let y = Math.random() * 450
+        e.target.pos = new Vector(x, y)
+
+    }
+
+    sharkLeft(e) {
+        let x = Math.random() * -1200
+        let y = Math.random() * 450
+        e.target.pos = new Vector(x, y)
+
+    }
+
+    bubbleLeft(e) {
+        let x = Math.random() * 1200
+        let y = 600 + Math.random() * 100
+        e.target.pos = new Vector(x, y)
+
     }
 }
 
--- src/js/resources.js
+++ src/js/resources.js
@@ -2,7 +2,9 @@ import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 
 // voeg hier jouw eigen resources toe
 const Resources = {
-    Fish: new ImageSource('images/fish.png')
+    Fish: new ImageSource('images/fish.png'),
+    Shark: new ImageSource('images/shark.png'),
+    Bubble: new ImageSource('images/bubble.png'),
 }
 
 
